7ed0a682891ec876c16ecf435a40127932d0bf5a
Andrew Miller <mail@andrewjm.co>
2019-08-17 23:16:48 -0500

    ugly commit. functioning prototype. adding phases and rows

---
+++ scratch_pad/19Q3_ShreveportConcert/html.js
@@ -0,0 +1,13 @@
+// future enhancement:
+// use setup a server (probably express) to fetch() local html
+// files from. this will allow me to edit only the primary
+// html file, without having to save as a string here
+//
+// https://stackoverflow.com/questions/50007055/fetch-request-to-local-file-not-working
+
+var improvisation_html = "improvisation";
+var triangle_html      = "triangle";
+
+var phases_html        = '<div class="phase-one"><div class="performer a"><div class="bar a"></div><div class="bar b"></div><div class="bar c"></div></div><div class="performer b"><div class="bar d"></div><div class="bar e"></div><div class="bar f"></div></div><div class="performer c"><div class="bar g"></div><div class="bar h"></div><div class="bar i"></div></div></div>';
+
+var rows_html          = '<div id="inst1" class="row"></div><div id="inst2" class="row"></div><div id="inst3" class="row"></div>';
