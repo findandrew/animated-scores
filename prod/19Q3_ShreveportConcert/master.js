04c51861f2a0cb6465fcd33334570b15b6f04ce8
Andrew Miller <mail@andrewjm.co>
2020-03-29 14:27:09 -0500

    move methods to utilities.js

--- prod/19Q3_ShreveportConcert/master.js
+++ prod/19Q3_ShreveportConcert/master.js
@@ -1,3 +1,5 @@
+// master
+
 document.addEventListener("DOMContentLoaded", function(event) {
 
   document.getElementById("start").onclick = manageSet;
@@ -47,59 +49,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // window.setTimeout(fadeIn,            24010, body);
 
     // window.setTimeout(fadeOut,           28000, body);
-  }
-
-  // utilities
-  function fadeOut(el) {
-    // could pass animation duration as a variable
-    el.style.animationPlayState = "paused";
-    el.style.animationName = "fadeout";
-    el.style.animationPlayState = "running";
-  }
-
-  function fadeIn(el) {
-    // could pass animation duration as a variable
-    el.style.animationPlayState = "paused";
-    el.style.animationName = "fadein";
-    el.style.animationPlayState = "running";
-  }
-
-  function clearHtml(el) {
-    el.innerHTML = "";
-  }
-
-  function loadCss(filename){
-    var fileref=document.createElement("link")
-    fileref.setAttribute("rel", "stylesheet")
-    fileref.setAttribute("type", "text/css")
-    fileref.setAttribute("href", filename)
-    document.getElementsByTagName("head")[0].appendChild(fileref)
-  }
-
-  function removeCss(filename){
-    var el=document.getElementsByTagName("link")
-
-    for (var i=el.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
-      if (el[i] && el[i].getAttribute("href")!=null && el[i].getAttribute("href").indexOf(filename)!=-1)
-          el[i].parentNode.removeChild(el[i]) //remove element
-    }
-  }
-
-  function buildPiece(el, html_var, css_file) {
-    el.innerHTML = html_var;
-    loadCss(css_file)
-  }
-
-  function startPiece(el, anim_elems) {
-    var el = document.querySelectorAll(anim_elems);
-    el.forEach( function(el) {
-      el.style.animationPlayState = "running";
-    })
-  }
-
-  function destroyPiece(el, css_file) {
-    el.innerHTML = "";
-    removeCss(css_file)
   }
 
 });
